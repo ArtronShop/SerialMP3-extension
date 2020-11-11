e73e56fbbbbeed31c3bd6ab03e497a4b2007f935
Max <sonthaya.nt@hotmail.com>
2020-11-11 23:49:23 +0700

    Fixed and Add shadow

--- extension.js
+++ extension.js
@@ -7,11 +7,18 @@
     icon: "/static/icon.png", // Category icon
     color: "#E74C3C", // Category color (recommend some blocks color)
     blocks: [ // Blocks in Category
-        "mp3_begin",
+        {
+            xml: `
+                <block type="mp3_begin">
+                    <field name="tx_pin">26</field>
+                    <field name="rx_pin">27</field>
+                </block>
+            `
+        },
         {
             xml: `
                 <block type="mp3_play_index">
-                    <value name="value">
+                    <value name="index">
                         <shadow type="math_number">
                             <field name="NUM">1</field>
                         </shadow>
@@ -21,7 +28,17 @@
         },
         "mp3_is_playing",
         "mp3_wait_play_end",
-        "mp3_set_volume",
+        {
+            xml: `
+                <block type="mp3_set_volume">
+                    <value name="level">
+                        <shadow type="math_number">
+                            <field name="NUM">50</field>
+                        </shadow>
+                    </value>
+                </block>
+            `
+        },
         "mp3_pause",
         "mp3_resume",
         "mp3_stop",
